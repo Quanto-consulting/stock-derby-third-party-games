aee46433b162442d4da8e2bbfc132354b02e0609
keyur <guptakeyur3@gmail.com>
2025-07-25 17:58:40 +0530

    deposit route updated

--- src/app/api/deposit/route.ts
+++ src/app/api/deposit/route.ts
@@ -4,6 +4,8 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   // You can process the request body here if needed
   const { userId, amount } = await req.json();
+  console.log("userId is: ", userId);
+  console.log("amount is: ", amount);
   if (!userId || !amount) {
     return NextResponse.json(
       { error: "userId and amount are required" },
