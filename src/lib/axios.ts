35a3ad47376d70610fac90c8370dd18d36656095
QuantoRahul <rahulrawat.quanto@gmail.com>
2025-09-14 22:49:17 +0530

    test

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,11 +1,10 @@
 import axios, { AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
-  baseURL: "https://stock-derby-third-party-games.vercel.app/api",
+  baseURL: process.env.EXTERNAL_BACKEND_URL ?? "/api",
   headers: {
     "Content-Type": "application/json",
     "api-key": process.env.API_KEY,
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Mywicm9sZSI6InVzZXIiLCJjb21wYW55SWQiOjQsImlhdCI6MTc1NzUwMzQwMCwiZXhwIjoxNzYwMDk1NDAwfQ.qSvl0dfuVHiBBqpVjYio7IdFkqIsNkn_Zge8zPSniJA",
   },
   withCredentials: true,
 });
